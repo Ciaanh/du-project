0169affe0793a7eb65b11bfb4d60291e40aa6019
Ciaanh <nicolas.fabre.21@outlook.fr>
2019-05-05 22:22:14 +0200

    save project

--- src/projectView/app/components/Project.tsx
+++ src/projectView/app/components/Project.tsx
@@ -1,239 +1,247 @@
-import * as React from 'react';
-
+import * as React from "react";
 
 // @ts-ignore
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { IHandler } from '../interfaces/dumodel';
-import { duProject } from '../interfaces/vsmodel';
-import { IMessage } from '../interfaces/messages';
-import { SlotIndexes } from '../interfaces/slotIndexes';
-
-
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { IHandler } from "../interfaces/dumodel";
+import { duProject } from "../interfaces/vsmodel";
+import { IMessage } from "../interfaces/messages";
+import { SlotIndexes } from "../interfaces/slotIndexes";
+import { OverviewMode } from "../interfaces/enums";
 
 interface IProjectProps {
-    vscode: any;
-    initialData: duProject;
+  vscode: any;
+  initialData: duProject;
 }
 
 interface IProjectState {
-    duProject: duProject;
-    currentSlot?: number;
-    currentHandler?: IHandler;
+  duProject: duProject;
+  currentSlot?: number;
+  currentHandler?: IHandler;
 }
 
-export default class Project extends React.Component<IProjectProps, IProjectState> {
+export default class Project extends React.Component<
+  IProjectProps,
+  IProjectState
+> {
+  constructor(props: any) {
+    super(props);
 
-    constructor(props: any) {
-        super(props);
+    let initialData = this.props.initialData;
 
-        let initialData = this.props.initialData;
-
-        let oldState = this.props.vscode.getState();
-        if (oldState) {
-            this.state = oldState;
-        }
-        else {
-            this.state = { duProject: initialData };
-        }
+    let oldState = this.props.vscode.getState();
+    if (oldState) {
+      this.state = oldState;
+    } else {
+      this.state = { duProject: initialData };
     }
+  }
 
-    componentDidMount() {
-        window.addEventListener('message', event => {
-            const message: IMessage = event.data;
-            switch (message.command) {
-                case 'initialize':
-                    this.defineState({ ...this.state, project: message.data })
-                    break;
-            }
-        });
+  componentDidMount() {
+    window.addEventListener("message", event => {
+      const message: IMessage = event.data;
+      switch (message.command) {
+        case "initialize":
+          this.defineState({ ...this.state, project: message.data });
+          break;
+      }
+    });
+  }
+
+  private defineState(newSate: any) {
+    this.setState(newSate);
+    this.props.vscode.setState(newSate);
+  }
+
+  private onSelectSlot(slotIndex: number) {
+    this.defineState({
+      ...this.state,
+      currentSlot: slotIndex,
+      currentHandler: undefined
+    });
+  }
+
+  private onSelectHandler(handler: IHandler) {
+    this.defineState({ ...this.state, currentHandler: handler });
+  }
+
+  private getHandlersBySlot(slotIndex: number): IHandler[] {
+    return this.state.duProject.project.handlers.filter(handler => {
+      return handler.filter.slotKey == slotIndex;
+    });
+  }
+
+  private editHandler(handler: IHandler) {
+    this.props.vscode.postMessage({
+      command: "editHandler",
+      slotKey: handler.filter.slotKey,
+      handlerKey: handler.key
+    });
+  }
+
+  // private fixMethodError(methodError: methodFileError) {
+  //     this.props.vscode.postMessage({
+  //         command: 'fixMethodError',
+  //         methodError: methodError
+  //     });
+  // }
+
+  private shouldHandleErrors(): boolean {
+    if (this.state.duProject.overviewMode === OverviewMode.Project) {
+      return true;
     }
+    return false;
+  }
 
-    private defineState(newSate: any) {
-        this.setState(newSate)
-        this.props.vscode.setState(newSate);
-    }
+  private renderFilterList(slotIndex: number) {
+    let handlers: IHandler[] = this.getHandlersBySlot(slotIndex);
 
-    private onSelectSlot(slotIndex: number) {
-        this.defineState({ ...this.state, currentSlot: slotIndex, currentHandler: undefined })
-    }
+    return (
+      <ul className="">
+        {handlers && handlers.length > 0
+          ? handlers.map((handler, handlerIndex) => {
+              let argList =
+                handler.filter.args && handler.filter.args.length > 0
+                  ? handler.filter.args
+                      .map(arg => {
+                        return arg.value;
+                      })
+                      .join(",")
+                  : null;
 
-    private onSelectHandler(handler: IHandler) {
-        this.defineState({ ...this.state, currentHandler: handler })
-    }
+              return (
+                <li
+                  key={handler.key}
+                  className="nav-item"
+                  onClick={() => this.onSelectHandler(handler)}
+                >
+                  <a
+                    className={
+                      "nav-link" +
+                      (this.state.currentHandler &&
+                      this.state.currentHandler.key === handler.key
+                        ? " active"
+                        : "")
+                    }
+                    href="#"
+                  >
+                    {handler.filter.signature} : {argList}
+                  </a>
+                </li>
+              );
+            })
+          : null}
+      </ul>
+    );
+  }
 
-    private getHandlersBySlot(slotIndex: number): IHandler[] {
-        return this.state.duProject.project.handlers.filter(handler => {
-            return handler.filter.slotKey == slotIndex;
-        });
-    }
+  private renderHandler(handler: IHandler) {
+    return (
+      <div className="">
+        <a className="editHandler" onClick={() => this.editHandler(handler)}>
+          Edit this code
+        </a>
+        <SyntaxHighlighter language="lua">{handler.code}</SyntaxHighlighter>
+      </div>
+    );
+  }
 
-    private editHandler(handler: IHandler) {
-        this.props.vscode.postMessage({
-            command: 'editHandler',
-            slotKey: handler.filter.slotKey,
-            handlerKey: handler.key
-        });
-    }
+  private renderSlotList() {
+    return (
+      <ul className="nav flex-column">
+        {this.state.duProject
+          ? SlotIndexes.indexes.map(slotIndex => {
+              let slot = this.state.duProject.project.slots[slotIndex];
+              if (slot) {
+                return (
+                  <li
+                    key={slotIndex}
+                    className="nav-item"
+                    onClick={() => this.onSelectSlot(slotIndex)}
+                  >
+                    <a
+                      className={
+                        "nav-link" +
+                        (this.state.currentSlot &&
+                        this.state.currentSlot === slotIndex
+                          ? " active"
+                          : "")
+                      }
+                      href="#"
+                    >
+                      {slot.name}
+                    </a>
+                  </li>
+                );
+              }
+              return (
+                <li key={slotIndex} className="nav-item">
+                  <span>Slot {slotIndex} is missing.</span>>
+                </li>
+              );
+            })
+          : null}
+      </ul>
+    );
+  }
 
-
-
-
-    // private fixMethodError(methodError: methodFileError) {
-    //     this.props.vscode.postMessage({
-    //         command: 'fixMethodError',
-    //         methodError: methodError
-    //     });
-    // }
-
-
-
-
-
-
-
-
-
-    private renderFilterList(slotIndex: number) {
-
-        let handlers: IHandler[] = this.getHandlersBySlot(slotIndex);
-
-        return (
-            <ul className="">
-                {(handlers && handlers.length > 0)
-                    ? handlers.map(
-                        (handler, handlerIndex) => {
-                            let argList = (handler.filter.args && handler.filter.args.length > 0)
-                                ? handler.filter.args.map((arg) => { return arg.value; }).join(",")
-                                : null;
-
-                            return (
-                                <li key={handler.key}
-                                    className="nav-item"
-                                    onClick={() => this.onSelectHandler(handler)}>
-
-                                    <a className={"nav-link" + ((this.state.currentHandler && this.state.currentHandler.key === handler.key) ? " active" : "")} href="#">
-                                        {handler.filter.signature} : {argList}
-                                    </a>
-
-                                </li>);
-                        })
-                    : null}
-            </ul>);
-    }
-
-    private renderHandler(handler: IHandler) {
-        return (
-            <div className="" >
-                <a className="editHandler" onClick={() => this.editHandler(handler)}>Edit this code</a>
-                <SyntaxHighlighter language='lua'>{handler.code}</SyntaxHighlighter>
-            </div>);
-    }
-
-
-    private renderSlotList() {
-        return (
-
-            <ul className="nav flex-column">
-                {
-                    (this.state.duProject)
-                        ? SlotIndexes.indexes.map(
-                            (slotIndex) => {
-                                let slot = this.state.duProject.project.slots[slotIndex];
-                                if (slot) {
-                                    return (
-                                        <li key={slotIndex}
-                                            className="nav-item"
-                                            onClick={() => this.onSelectSlot(slotIndex)}>
-
-                                            <a className={"nav-link" + ((this.state.currentSlot && this.state.currentSlot === slotIndex) ? " active" : "")}
-                                                href="#">{slot.name}
-                                            </a>
-
-                                        </li>);
-                                }
-                                return (
-                                    <li key={slotIndex}
-                                        className="nav-item" >
-                                        <span>Slot {slotIndex} is missing.</span>>
-                                            </li>);
-                            })
-                        : null}
-            </ul>
-        );
-    }
-
-
-
-    render() {
-
-        return (
-            <React.Fragment>
-
-                <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-                    <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">DU Project - json to lua overview</a>
-                    {/* <input className="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search" />
+  render() {
+    return (
+      <React.Fragment>
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+          <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">
+            DU Project - json to lua overview
+          </a>
+          {/* <input className="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search" />
                     <ul className="navbar-nav px-3">
                         <li className="nav-item text-nowrap">
                             <a className="nav-link" href="#">Sign out</a>
                         </li>
                     </ul> */}
-                </nav>
+        </nav>
 
-                <div className="container-fluid">
-                    <div className="row">
-                        <nav className="col-2 sidebar">
-                            <div className="sidebar-sticky">
-                                {
-                                    this.renderSlotList()
-                                }
-                            </div>
-                        </nav>
+        <div className="container-fluid">
+          <div className="row">
+            <nav className="col-2 sidebar">
+              <div className="sidebar-sticky">{this.renderSlotList()}</div>
+            </nav>
 
-                        <nav className="col-2 sidebar bis">
-                            <div className="sidebar-sticky">
-                                {
-                                    (this.state.currentSlot)
-                                        ? this.renderFilterList(this.state.currentSlot)
-                                        : null
-                                }
-                            </div>
-                        </nav>
+            <nav className="col-2 sidebar bis">
+              <div className="sidebar-sticky">
+                {this.state.currentSlot
+                  ? this.renderFilterList(this.state.currentSlot)
+                  : null}
+              </div>
+            </nav>
 
-                        <main className="col-8 code">
-                            {
-                                (this.state.currentHandler)
-                                    ? this.renderHandler(this.state.currentHandler)
-                                    : null
-                            }
-
-                        </main>
-                    </div>
-                </div>
-            </React.Fragment>
-        );
-    }
+            <main className="col-8 code">
+              {this.state.currentHandler
+                ? this.renderHandler(this.state.currentHandler)
+                : null}
+            </main>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
-                // let currentCount = (oldState && oldState.count) || 0;
+// let currentCount = (oldState && oldState.count) || 0;
 
-                    // // Update state
-    // vscode.setState({count: currentCount });
+// // Update state
+// vscode.setState({count: currentCount });
 
-    // this.props.vscode.postMessage({
-    //     command: 'alert',
-    //     text: '🐛  we welcomed ' + this.props.name
-    // });
+// this.props.vscode.postMessage({
+//     command: 'alert',
+//     text: '🐛  we welcomed ' + this.props.name
+// });
 
-    // // Handle messages sent from the extension to the webview
-    // window.addEventListener('message', event => {
-    //     const message = event.data; // The json data that the extension sent
-    //     switch (message.command) {
-    //         case 'refactor':
-    //             currentCount = Math.ceil(currentCount * 0.5);
-    //             counter.textContent = currentCount;
-    //             break;
-    //     }
-    // });
-
-
-
+// // Handle messages sent from the extension to the webview
+// window.addEventListener('message', event => {
+//     const message = event.data; // The json data that the extension sent
+//     switch (message.command) {
+//         case 'refactor':
+//             currentCount = Math.ceil(currentCount * 0.5);
+//             counter.textContent = currentCount;
+//             break;
+//     }
+// });
